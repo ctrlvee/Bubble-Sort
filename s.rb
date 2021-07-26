7210698af97ac24ad8dfb22170cc3f0c0bce5ee5
ctrlVee <me123opportunity@gmail.com>
2021-07-26 14:22:05 -0400

    Almost completes a whole iteration of sorting - need to fix get_index

--- s.rb
+++ s.rb
@@ -8,6 +8,7 @@ require 'pry-byebug'
 # 1; a > b
 
 def compare_values(a,b)
+  #Depending on the value here, it will reassign a in the main function
   case a<=>b
   when -1 
     return "-1"
@@ -15,6 +16,7 @@ def compare_values(a,b)
     return "0"
   when 1 
     switch_index(a,b)
+    return "1"
   end
 end
 
@@ -30,12 +32,20 @@ def switch_index(a,b)
 end
 
 @array.reduce() do |a, b|
+  #Messes up when it reaches the last #2 because get_index finds the first occurence of "2" which is in index = 2. So it switches with that two instead.
   binding.pry
   #Compare A & B. 
   #If a > b: swap index positions
   #Then assign b = a since the next iteration, b will change to the next value
-  compare_values(a,b)
+  case compare_values(a,b)
+  when "-1"
+    a = b
+  when "0"
+    a = b
+  when "1"
+    a = a
+  end
 
-  puts "A: #{a} B: #{b}"
+  #puts "A: #{a} B: #{b}"
 end
 
