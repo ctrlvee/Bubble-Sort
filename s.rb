cb7b17448ffbf258863247ff29f25ed42b3dab88
ctrlVee <me123opportunity@gmail.com>
2021-07-26 14:03:09 -0400

    Gets index + switches the index if <=> returns 1

--- s.rb
+++ s.rb
@@ -1,14 +1,39 @@
 require 'pry-byebug'
 
-array = [4,3,78,2,0,2]
+@array = [4,3,78,2,0,2]
 
-#binding.pry
 
-array.reduce() do |a, b|
 
+
+def compare_values(a,b)
+  case a<=>b
+  when -1
+    return "-1"
+  when 0
+    return "0"
+  when 1
+    switch_index(a,b)
+  end
+end
+
+def get_index(a,b)
+  @a_index = @array.index(a)
+  @b_index = @array.index(b)
+end
+
+def switch_index(a,b)
+  get_index(a,b)
+  @array[@a_index] = b
+  @array[@b_index] = a
+end
+
+@array.reduce() do |a, b|
+  binding.pry
   #Compare A & B. 
   #If a > b: swap index positions
   #Then assign b = a since the next iteration, b will change to the next value
-  
-puts "A: #{a} B: #{b}"
+  compare_values(a,b)
+
+  puts "A: #{a} B: #{b}"
 end
+
